05603d789d5b61f24cacb97564ede9a40f51440d
purplenicole730 <jihyun.nicole.jung@gmail.com>
2023-04-27 14:21:56 -0400

    Revert "add button and led logic"
    This reverts commit 44090d23b7a1190681676eee1fc6fb62719480e8.

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 import asyncio
-import time
 
 from viam.robot.client import RobotClient
-from viam.rpc.dial import DialOptions
+from viam.rpc.dial import Credentials, DialOptions
 from viam.components.board import Board
 from viam.components.servo import Servo
 
@@ -17,34 +16,29 @@ async def connect():
     )
     return await RobotClient.at_address(secrets.address, opts)
 
-
 async def main():
     robot = await connect()
 
     print("Resources:")
     print(robot.resource_names)
 
+    # Note that the pin supplied is a placeholder. Please change this to a valid pin you are using.
+    # pi
     pi = Board.from_robot(robot, "pi")
-    button = await pi.gpio_pin_by_name("18")
-    led = await pi.gpio_pin_by_name("16")
+    pi_return_value = await pi.gpio_pin_by_name("16")
+    print(f"pi gpio_pin_by_name return value: {pi_return_value}")
 
-    start = time.time()
-    while time.time() < start + 10.0:
-        button_state = await button.get()
-        old_state = button_state
-        if button_state != old_state:
-            print("button state has changed!")
-        await led.set(button_state)
-
+    # Note that the pin supplied is a placeholder. Please change this to a valid pin you are using.
     # pca
-    # pca = Board.from_robot(robot, "pca")
-    # pca_return_value = await pca.gpio_pin_by_name("16")  # placeholder pin
-    # print(f"pca gpio_pin_by_name return value: {pca_return_value}")
+    pca = Board.from_robot(robot, "pca")
+    pca_return_value = await pca.gpio_pin_by_name("16")
+    print(f"pca gpio_pin_by_name return value: {pca_return_value}")
 
     # servo
     servo = Servo.from_robot(robot, "servo")
     servo_return_value = await servo.get_position()
     print(f"servo get_position return value: {servo_return_value}")
+
 
     # Don't forget to close the robot when you're done!
     await robot.close()
