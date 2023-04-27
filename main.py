df7373f2aea8f6d3a4553b8f842ef819e077bc12
Nicole Jung <31713368+purplenicole730@users.noreply.github.com>
2023-04-27 14:33:07 -0400

    Add button and led logic script (#2)
    * basic cleanup for actually running code
    
    * add 10 second basic script to turn led on when button pressed

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 import asyncio
+import time
 
 from viam.robot.client import RobotClient
-from viam.rpc.dial import Credentials, DialOptions
+from viam.rpc.dial import DialOptions
 from viam.components.board import Board
 from viam.components.servo import Servo
 
@@ -16,29 +17,34 @@ async def connect():
     )
     return await RobotClient.at_address(secrets.address, opts)
 
+
 async def main():
     robot = await connect()
 
     print("Resources:")
     print(robot.resource_names)
 
-    # Note that the pin supplied is a placeholder. Please change this to a valid pin you are using.
-    # pi
     pi = Board.from_robot(robot, "pi")
-    pi_return_value = await pi.gpio_pin_by_name("16")
-    print(f"pi gpio_pin_by_name return value: {pi_return_value}")
+    button = await pi.gpio_pin_by_name("18")
+    led = await pi.gpio_pin_by_name("16")
 
-    # Note that the pin supplied is a placeholder. Please change this to a valid pin you are using.
+    start = time.time()
+    while time.time() < start + 10.0:
+        button_state = await button.get()
+        old_state = button_state
+        if button_state != old_state:
+            print("button state has changed!")
+        await led.set(button_state)
+
     # pca
-    pca = Board.from_robot(robot, "pca")
-    pca_return_value = await pca.gpio_pin_by_name("16")
-    print(f"pca gpio_pin_by_name return value: {pca_return_value}")
+    # pca = Board.from_robot(robot, "pca")
+    # pca_return_value = await pca.gpio_pin_by_name("16")  # placeholder pin
+    # print(f"pca gpio_pin_by_name return value: {pca_return_value}")
 
     # servo
     servo = Servo.from_robot(robot, "servo")
     servo_return_value = await servo.get_position()
     print(f"servo get_position return value: {servo_return_value}")
-
 
     # Don't forget to close the robot when you're done!
     await robot.close()
