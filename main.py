f4348ae683ad7cf2a9672a0acd207afa0724d585
purplenicole730 <jihyun.nicole.jung@gmail.com>
2023-04-27 14:17:12 -0400

    basic cleanup for actually running code

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import asyncio
 
 from viam.robot.client import RobotClient
-from viam.rpc.dial import Credentials, DialOptions
+from viam.rpc.dial import DialOptions
 from viam.components.board import Board
 from viam.components.servo import Servo
 
@@ -16,29 +16,24 @@ async def connect():
     )
     return await RobotClient.at_address(secrets.address, opts)
 
+
 async def main():
     robot = await connect()
 
     print("Resources:")
     print(robot.resource_names)
 
-    # Note that the pin supplied is a placeholder. Please change this to a valid pin you are using.
-    # pi
     pi = Board.from_robot(robot, "pi")
-    pi_return_value = await pi.gpio_pin_by_name("16")
-    print(f"pi gpio_pin_by_name return value: {pi_return_value}")
 
-    # Note that the pin supplied is a placeholder. Please change this to a valid pin you are using.
     # pca
-    pca = Board.from_robot(robot, "pca")
-    pca_return_value = await pca.gpio_pin_by_name("16")
-    print(f"pca gpio_pin_by_name return value: {pca_return_value}")
+    # pca = Board.from_robot(robot, "pca")
+    # pca_return_value = await pca.gpio_pin_by_name("16")  # placeholder pin
+    # print(f"pca gpio_pin_by_name return value: {pca_return_value}")
 
     # servo
     servo = Servo.from_robot(robot, "servo")
     servo_return_value = await servo.get_position()
     print(f"servo get_position return value: {servo_return_value}")
-
 
     # Don't forget to close the robot when you're done!
     await robot.close()
