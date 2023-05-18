447a1ae19c1459736c5f1f79df1c7da0d27ccf69
Alan Davidson <alan@viam.com>
2023-05-18 12:30:41 -0400

    non-public things start with underscores

--- main.py
+++ main.py
@@ -23,74 +23,74 @@ class Robot:
             refresh_interval=0,
             dial_options=DialOptions(credentials=secrets.creds)
         )
-        self.robot = await RobotClient.at_address(secrets.address, opts)
-        self.servo = Servo.from_robot(self.robot, "servo")
+        self._robot = await RobotClient.at_address(secrets.address, opts)
+        self._servo = Servo.from_robot(self._robot, "servo")
 
-        self.mutex = threading.Lock()
-        self.count = 0  # Number of hands currently raised
+        self._mutex = threading.Lock()
+        self._count = 0  # Number of hands currently raised
 
         # We have a background thread that wiggles the hand when it's been
         # raised for a long time. This condition variable is how to shut that
         # down.
-        # It would be intuitive to pass self.mutex to threading.Condition, but
+        # It would be intuitive to pass self._mutex to threading.Condition, but
         # that will result in deadlock when you have the mutex and wait for the
         # thread to shut down.
-        self.cv = threading.Condition()
-        self.should_shutdown_thread = True
-        self.thread = None
+        self._cv = threading.Condition()
+        self._should_shutdown_thread = True
+        self._thread = None
 
     def _start_thread(self):
-        self.should_shutdown_thread = False
-        self.thread = threading.Thread(
+        self._should_shutdown_thread = False
+        self._thread = threading.Thread(
                 target=asyncio.run,
                 args=(self._wiggle_on_inactivity,),
                 daemon=True)
-        self.thread.start()
+        self._thread.start()
 
     def _stop_thread(self(:
-        self.should_shutdown_thread = True
-        self.cv.notify()
-        self.thread.join()
+        self._should_shutdown_thread = True
+        self._cv.notify()
+        self._thread.join()
 
     async def raise_hand(self):
-        with self.mutex:
-            self.count += 1
-            await self.servo.move(self.UPPER_POSITION)
-            if self.count == 1:
+        with self._mutex:
+            self._count += 1
+            await self._servo.move(self.UPPER_POSITION)
+            if self._count == 1:
                 self._start_thread()
 
     async def lower_hand(self):
-        with self.mutex:
-            self.count -= 1
-            if self.count == 0:
-                await self.servo.move(self.LOWER_POSITION)
+        with self._mutex:
+            self._count -= 1
+            if self._count == 0:
+                await self._servo.move(self.LOWER_POSITION)
                 self._stop_thread()
 
     async def set_count(self, new_value):
-        with self.mutex:
-            if self.count == 0 and new_value > 0:
+        with self._mutex:
+            if self._count == 0 and new_value > 0:
                 self._start_thread()
-            self.count = new_value
-            if self.count == 0:
-                await self.servo.move(self.LOWER_POSITION)
+            self._count = new_value
+            if self._count == 0:
+                await self._servo.move(self.LOWER_POSITION)
                 self._stop_thread()
             else:
-                await self.servo.move(self.UPPER_POSITION)
+                await self._servo.move(self.UPPER_POSITION)
 
     async def _wiggle_hand(self):
         for _ in range(3):
-            await self.servo.move(self.UPPER_POSITION + self.WIGGLE_AMOUNT)
+            await self._servo.move(self.UPPER_POSITION + self.WIGGLE_AMOUNT)
             time.sleep(0.3)
-            await self.servo.move(self.UPPER_POSITION)
+            await self._servo.move(self.UPPER_POSITION)
             time.sleep(0.3)
 
     async def _wiggle_on_inactivity(self):
         # This is run in a daemon thread. When the hand is raised and nothing
         # has happened for INACTIVITY_PERIOD_S seconds, we wiggle the hand.
-        with self.cv:
-            while not self.should_shutdown_thread:
-                self.cv.wait(timeout=INACTIVITY_PERIOD_S)
-                if self.should_shutdown_thread:
+        with self._cv:
+            while not self._should_shutdown_thread:
+                self._cv.wait(timeout=INACTIVITY_PERIOD_S)
+                if self._should_shutdown_thread:
                     return
                 await self._wiggle_hand()
 
