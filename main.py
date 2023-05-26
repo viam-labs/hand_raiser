3547a247b7aeb7a02ee572bcd2e21ced38f14cd1
Alan Davidson <alan@viam.com>
2023-05-26 16:38:49 -0400

    Count people, wiggle hand when idle (#5)
    The class now stores how many people have their hand raised: you can increment or decrement this value, or set it to some specific value (which might be unused in the final product, but will help with a PoC before we integrate with Zoom). When the hand has been raised for sufficiently long, it wiggles (or is supposed to, see below).
    
    The current main function now uses the button to toggle between 1 person raising and lowering the hand (so, it doesn't actually demonstrate that we do the right thing multiple people yet). I imagine the next PR after this will be to add a server that takes in requests to raise and lower the hand, at which point we can get rid of the button and the busy loop, and demonstrate support for multiple people (and also enable remote folks to play with the code, since you no longer need to push the button).
    
    
    * start on a thread to wiggle the hand on inactivity
    
    * names of non-public functions should start with an underscore
    
    * start and stop the thread as relevant when setting the count, too
    
    * The condition variable should have its own mutex
    
    * simplify main function: button just raises and lowers, since wiggling is idle now
    
    * non-public things start with underscores
    
    * Make self._thread None when the thread isn't supposed to be running
    
    * avoid race condition: don't start the thread until the hand is raised
    
    * fix syntax error
    
    * the robot is not a private member (yet)
    
    * debugging
    
    * avoid race condition: always stop the thread before lowering the hand
    
    * tweak: make the code look symmetric
    
    * avoid deadlock: only acquire the condition variable when you're using it
    
    * lock the mutex whenever you interact with viam. Maybe not needed?
    
    * use coroutines instead of threads, needs debugging info removed when ready
    
    * add docstrings
    
    * move _wiggle_on_inactivity up near _start_wiggler and _stop_wiggler
    
    * debugging: await coroutines
    
    * debugging
    
    * more debugging
    
    * try an Event instead of a condition variable
    
    * things work, so cleaning up
    
    * only use the mutex when we mutate state
    
    * use asyncio.sleep instead of time.sleep
    
    * tweak comments
    
    * remove more debugging logic
    
    * cancel the task, rather than setting an event to tell the task to cancel itself
    
    * Add another docstring
    
    * debugging

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 import asyncio
 import contextlib
-import time
 
 from viam.robot.client import RobotClient
 from viam.rpc.dial import DialOptions
@@ -15,27 +14,101 @@ class Robot:
     UPPER_POSITION = 30
     LOWER_POSITION = 0
     WIGGLE_AMOUNT = 5
+    INACTIVITY_PERIOD_S = 5
 
     async def connect(self):
+        """
+        This function does the initialization that would normally go in
+        __init__(), except we need asyncio functionality, so we put it here
+        instead.
+        """
         opts = RobotClient.Options(
             refresh_interval=0,
             dial_options=DialOptions(credentials=secrets.creds)
         )
         self.robot = await RobotClient.at_address(secrets.address, opts)
-        self.servo = Servo.from_robot(self.robot, "servo")
+        self._servo = Servo.from_robot(self.robot, "servo")
+
+        self._mutex = asyncio.Lock()
+        self._count = 0  # Number of hands currently raised
+
+        # self._wiggler will become an asyncio.Task when the hand is raised. It
+        # will wiggle the hand when it has been raised for over
+        # INACTIVITY_PERIOD_S seconds.
+        self._wiggler = None
+
+    def _start_wiggler(self):
+        self._wiggler = asyncio.create_task(self._wiggle_on_inactivity())
+
+    async def _stop_wiggler(self):
+        self._wiggler.cancel()
+        await self._wiggler
+        self._wiggler = None
+
+    async def _wiggle_on_inactivity(self):
+        """
+        This is a background coroutine that wiggles the hand every
+        INACTIVITY_PERIOD_S seconds. It is started when the count of raised
+        hands becomes nonzero, and canceled when the count goes back to 0.
+        """
+        try:
+            while True:
+                await asyncio.sleep(self.INACTIVITY_PERIOD_S)
+                await self._wiggle_hand()
+        except asyncio.CancelledError:
+            return
 
     async def raise_hand(self):
-        await self.servo.move(self.UPPER_POSITION)
+        """
+        Call this to consider 1 extra person in the audience to have raised
+        their hand. If this is the first person to do so, we'll raise our
+        servo, and otherwise we take no action.
+        """
+        async with self._mutex:
+            self._count += 1
+            if self._count == 1:
+                await self._servo.move(self.UPPER_POSITION)
+                self._start_wiggler()
 
     async def lower_hand(self):
-        await self.servo.move(self.LOWER_POSITION)
+        """
+        Call this to consider 1 extra person in the audience to have lowered
+        their hand. If this is the last person who had their hand raised, we'll
+        lower our servo, and otherwise take no action.
+        """
+        async with self._mutex:
+            self._count -= 1
+            if self._count == 0:
+                await self._stop_wiggler()
+                await self._servo.move(self.LOWER_POSITION)
 
-    async def wiggle_hand(self):
+    async def set_count(self, new_value):
+        """
+        Call this to set the number of hands raised in the audience to a certain
+        value. This is mainly used to "reset" the count of raised hands if
+        someone forgets to lower their hand.
+        """
+        async with self._mutex:
+            should_start_wiggler = self._count == 0 and new_value > 0
+            self._count = new_value
+            if self._count == 0:
+                await self._stop_wiggler()
+                await self._servo.move(self.LOWER_POSITION)
+            else:
+                await self._servo.move(self.UPPER_POSITION)
+                if should_start_wiggler:
+                    self._start_wiggler()
+
+    async def _wiggle_hand(self):
+        """
+        This moves the servo to wiggle the hand, intended to be used when we've
+        had the hand raised for a while.
+        """
         for _ in range(3):
-            await self.servo.move(self.UPPER_POSITION + self.WIGGLE_AMOUNT)
-            time.sleep(0.3)
-            await self.servo.move(self.UPPER_POSITION)
-            time.sleep(0.3)
+            await self._servo.move(self.UPPER_POSITION + self.WIGGLE_AMOUNT)
+            await asyncio.sleep(0.3)
+            await self._servo.move(self.UPPER_POSITION)
+            await asyncio.sleep(0.3)
 
 
 @contextlib.asynccontextmanager
@@ -54,19 +127,16 @@ async def main():
         button = await pi.gpio_pin_by_name("18")
         led = await pi.gpio_pin_by_name("16")
 
-        count = 0
+        should_raise = False
         old_state = False
         while True:
             button_state = await button.get()
             if button_state != old_state:
-                print("button state has changed!")
+                print("button state has changed to {}!".format(button_state))
                 if button_state:
-                    count += 1
-                    count %= 3
-                    if count == 1:
+                    should_raise = not should_raise
+                    if should_raise:
                         await robot.raise_hand()
-                    elif count == 2:
-                        await robot.wiggle_hand()
                     else:
                         await robot.lower_hand()
             old_state = button_state
