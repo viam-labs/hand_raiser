2a137127acf9fcea4f7b80b1e18764c6261a81c2
Alan Davidson <alan@viam.com>
2023-05-30 17:52:35 -0400

    refactor into two separate classes

--- main.py
+++ main.py
@@ -29,20 +29,9 @@ class Robot:
         self.robot = await RobotClient.at_address(secrets.address, opts)
         self._servo = Servo.from_robot(self.robot, "servo")
 
-        self._mutex = asyncio.Lock()
-        self._count = 0  # Number of hands currently raised
-
         # self._wiggler will become an asyncio.Task when the hand is raised. It
         # will wiggle the hand when it has been raised for over
         # INACTIVITY_PERIOD_S seconds.
-        self._wiggler = None
-
-    def _start_wiggler(self):
-        self._wiggler = asyncio.create_task(self._wiggle_on_inactivity())
-
-    async def _stop_wiggler(self):
-        self._wiggler.cancel()
-        await self._wiggler
         self._wiggler = None
 
     async def _wiggle_on_inactivity(self):
@@ -54,11 +43,41 @@ class Robot:
         try:
             while True:
                 await asyncio.sleep(self.INACTIVITY_PERIOD_S)
-                await self._wiggle_hand()
+                for _ in range(3):
+                    await self._servo.move(self.UPPER_POSITION + self.WIGGLE_AMOUNT)
+                    await asyncio.sleep(0.3)
+                    await self._servo.move(self.UPPER_POSITION)
+                    await asyncio.sleep(0.3)
         except asyncio.CancelledError:
             return
 
     async def raise_hand(self):
+        """
+        Call this to move the servo to the raised position and start the task
+        that wiggles the hand on inactivity.
+        """
+        await self._servo.move(self.UPPER_POSITION)
+        self._wiggler = asyncio.create_task(self._wiggle_on_inactivity())
+
+    async def lower_hand(self):
+        """
+        Call this to move the servo to the lowered position and stop the
+        background task that wiggles the hand once in a while.
+        """
+        self._wiggler.cancel()
+        await self._wiggler
+        self._wiggler = None
+        await self._servo.move(self.LOWER_POSITION)
+
+
+class Audience:
+    def __init__(self, robot):
+        self._robot = robot
+        self._mutex = asyncio.Lock()
+        self._count = 0  # Number of people in the audience with their hand raised
+        self._robot.lower_hand()
+
+    async def increment_count(self):
         """
         Call this to consider 1 extra person in the audience to have raised
         their hand. If this is the first person to do so, we'll raise our
@@ -67,10 +86,9 @@ class Robot:
         async with self._mutex:
             self._count += 1
             if self._count == 1:
-                await self._servo.move(self.UPPER_POSITION)
-                self._start_wiggler()
+                self._robot.raise_hand()
 
-    async def lower_hand(self):
+    async def decrement_count(self):
         """
         Call this to consider 1 extra person in the audience to have lowered
         their hand. If this is the last person who had their hand raised, we'll
@@ -79,8 +97,7 @@ class Robot:
         async with self._mutex:
             self._count -= 1
             if self._count == 0:
-                await self._stop_wiggler()
-                await self._servo.move(self.LOWER_POSITION)
+                self._robot.lower_hand()
 
     async def set_count(self, new_value):
         """
@@ -95,22 +112,9 @@ class Robot:
             self._count = new_value
 
             if should_raise_servo:
-                await self._servo.move(self.UPPER_POSITION)
-                self._start_wiggler()
+                self._robot.raise_hand()
             if should_lower_servo:
-                await self._stop_wiggler()
-                await self._servo.move(self.LOWER_POSITION)
-
-    async def _wiggle_hand(self):
-        """
-        This moves the servo to wiggle the hand, intended to be used when we've
-        had the hand raised for a while.
-        """
-        for _ in range(3):
-            await self._servo.move(self.UPPER_POSITION + self.WIGGLE_AMOUNT)
-            await asyncio.sleep(0.3)
-            await self._servo.move(self.UPPER_POSITION)
-            await asyncio.sleep(0.3)
+                self._robot.lower_hand()
 
 
 @contextlib.asynccontextmanager
@@ -125,6 +129,7 @@ async def makeRobot():
 
 async def main():
     async with makeRobot() as robot:
+        audience = Audience(robot)
         pi = Board.from_robot(robot.robot, "pi")
         button = await pi.gpio_pin_by_name("18")
         led = await pi.gpio_pin_by_name("16")
@@ -138,9 +143,9 @@ async def main():
                 if button_state:
                     should_raise = not should_raise
                     if should_raise:
-                        await robot.raise_hand()
+                        await audience.increment_count()
                     else:
-                        await robot.lower_hand()
+                        await audience.decrement_count()
             old_state = button_state
             await led.set(button_state)
 
