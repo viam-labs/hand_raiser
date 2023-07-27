2c20ac814391b5573688cbdce34fb0e331ed8108
Alan Davidson <alan@viam.com>
2023-07-27 16:01:23 -0400

    Refactoring: use contextlib and other tweaks (#8)
    No behavioral changes are intended. 
    
    Tried out on Nicole's desk: things work as intended, except that there are sometimes weird pauses in the middle of wiggling (e.g., instead of getting 3 wiggles, we get 1 wiggle, then a pause, and then 2 wiggles). I doubt that's caused by the hardware, and I don't see how it could be caused by this software, but I also feel uncomfortable just blaming gremlins in the wifi. It would be neat to try running everything on a single board and seeing if that gets rid of the delays...
    
    
    * rename Robot._robot -> Robot._client
    
    * add docstring
    
    * use contextlib instead of raw __aenter__ functions
    
    * split the context manager into helper functions
    
    * Pass in the credentials to the context manager. Hooray, pure functions!
    
    * add/tweak docstrings
    
    * rename pi -> board. It might not always be a pi.
    
    * Split the Robot class out to a separate file
    
    * tweak more docstrings
    
    * Move Robot.create out of the class, now that it's in a robot-specific file
    
    * tweaks
    
    * alphabetize imports
    
    * the class file doesn't need to be executable
    
    * debugging
    
    * fix docstring
    
    * better encapsulation: the robot only cares about the servo, and the board comes straight from the context manager
    
    * wrap long lines

--- main.py
+++ main.py
@@ -1,103 +1,25 @@
 #!/usr/bin/env python3
 import asyncio
-import contextlib
 
-from viam.robot.client import RobotClient
-from viam.rpc.dial import DialOptions
-from viam.components.board import Board
-from viam.components.servo import Servo
-
+from robot import create_robot
 import secrets
-
-
-class Robot:
-    UPPER_POSITION = 30
-    LOWER_POSITION = 0
-    WIGGLE_AMOUNT = 5
-    INACTIVITY_PERIOD_S = 5
-
-    async def __aenter__(self):
-        opts = RobotClient.Options(
-            refresh_interval=0,
-            dial_options=DialOptions(credentials=secrets.creds)
-        )
-        self._robot = await RobotClient.at_address(secrets.address, opts)
-        self._servo = Servo.from_robot(self._robot, "servo")
-
-        # This will become an asyncio.Task when the hand is raised. It will
-        # wiggle the hand when it has been raised for over INACTIVITY_PERIOD_S
-        # seconds.
-        self._wiggler = None
-
-        await self._servo.move(self.LOWER_POSITION)
-        return self
-
-    async def __aexit__(self, *exception_data):
-        if self._wiggler is not None:
-            await self.lower_hand()
-        await self._robot.close()
-
-    # TODO: remove this when we're ready
-    def get_pi(self):
-        return Board.from_robot(self._robot, "pi")
-
-    async def _wiggle_on_inactivity(self):
-        """
-        This is a background coroutine that wiggles the hand every
-        INACTIVITY_PERIOD_S seconds. It is started when the hand is raised,
-        and canceled when the hand is lowered.
-        """
-        try:
-            while True:
-                await asyncio.sleep(self.INACTIVITY_PERIOD_S)
-                for _ in range(3):
-                    await self._servo.move(self.UPPER_POSITION + self.WIGGLE_AMOUNT)
-                    await asyncio.sleep(0.3)
-                    await self._servo.move(self.UPPER_POSITION)
-                    await asyncio.sleep(0.3)
-        except asyncio.CancelledError:
-            return
-
-    async def raise_hand(self):
-        """
-        Call this to move the servo to the raised position and start the task
-        that wiggles the hand on inactivity.
-
-        Note: this function is not thread safe!
-        """
-        if self._wiggler is not None:
-            print("LOGIC BUG: trying to raise already-raised hand")
-            return
-        await self._servo.move(self.UPPER_POSITION)
-        self._wiggler = asyncio.create_task(self._wiggle_on_inactivity())
-
-    async def lower_hand(self):
-        """
-        Call this to move the servo to the lowered position and stop the
-        background task that wiggles the hand once in a while.
-
-        Note: this function is not thread safe!
-        """
-        if self._wiggler is None:
-            print("LOGIC BUG: trying to lower already-lowered hand")
-            return
-        self._wiggler.cancel()
-        await self._wiggler
-        self._wiggler = None
-        await self._servo.move(self.LOWER_POSITION)
 
 
 class Audience:
     def __init__(self, robot):
+        """
+        Audience keeps track of how many people have their hands up, and raises
+        and lowers the robot's hand to match. This class is thread safe.
+        """
         self._robot = robot
         self._mutex = asyncio.Lock()
-        self._count = 0  # Number of people in the audience with their hand raised
+        self._count = 0  # Number of people in the audience raising their hands
 
     async def increment_count(self):
         """
         Call this to consider 1 extra person in the audience to have raised
-        their hand. If this is the first person to do so, we'll raise our
-        servo, and otherwise we take no action.
+        their hand. If this is the first person to do so, we'll raise the
+        robot's hand, and otherwise we take no action.
         """
         async with self._mutex:
             self._count += 1
@@ -108,7 +30,7 @@ class Audience:
         """
         Call this to consider 1 extra person in the audience to have lowered
         their hand. If this is the last person who had their hand raised, we'll
-        lower our servo, and otherwise take no action.
+        lower the robot's hand, and otherwise take no action.
         """
         async with self._mutex:
             self._count -= 1
@@ -132,11 +54,10 @@ class Audience:
 
 
 async def main():
-    async with Robot() as robot:
+    async with create_robot(secrets.creds, secrets.address) as (robot, board):
         audience = Audience(robot)
-        pi = robot.get_pi()
-        button = await pi.gpio_pin_by_name("18")
-        led = await pi.gpio_pin_by_name("16")
+        button = await board.gpio_pin_by_name("18")
+        led = await board.gpio_pin_by_name("16")
 
         should_raise = False
         old_state = False
---
+++ robot.py
@@ -0,0 +1,107 @@
+import asyncio
+from contextlib import asynccontextmanager
+
+from viam.components.board import Board
+from viam.components.servo import Servo
+from viam.robot.client import RobotClient
+from viam.rpc.dial import DialOptions
+
+
+@asynccontextmanager
+async def create_robot(creds, address):
+    """
+    This makes a connection to the hardware, creates a Robot object, and then
+    closes the connection when the context manager exits.
+    """
+    opts = RobotClient.Options(
+        refresh_interval=0,
+        dial_options=DialOptions(credentials=creds)
+    )
+    client = await RobotClient.at_address(address, opts)
+    servo = Servo.from_robot(client, "servo")
+    board = Board.from_robot(client, "pi")
+
+    robot = Robot(servo)
+    await robot.start()
+    try:
+        # TODO: don't return the board when we no longer need to.
+        yield robot, board
+    finally:
+        await robot.stop()
+        await client.close()
+
+
+class Robot:
+    UPPER_POSITION = 30
+    LOWER_POSITION = 0
+    WIGGLE_AMOUNT = 5
+    INACTIVITY_PERIOD_S = 5
+
+    def __init__(self, servo):
+        """
+        This class is in charge of raising and lowering the robot's hand, and
+        wiggling the hand if it has been raised for too long.
+
+        WARNING: this class is not thread safe!
+        """
+        self._servo = servo
+
+        # This will become an asyncio.Task when the hand is raised. It will
+        # wiggle the hand when it has been raised for over INACTIVITY_PERIOD_S
+        # seconds.
+        self._wiggler = None
+
+    async def start(self):
+        """
+        Ideally, this would happen in __init__, but it needs to be async.
+        """
+        await self._servo.move(self.LOWER_POSITION)
+
+    async def stop(self):
+        """
+        Call this to ensure the hand is definitely lowered.
+        """
+        if self._wiggler is not None:
+            await self.lower_hand()
+
+    async def _wiggle_on_inactivity(self):
+        """
+        This is a background coroutine that wiggles the hand every
+        INACTIVITY_PERIOD_S seconds. It is started when the hand is raised,
+        and canceled when the hand is lowered.
+        """
+        try:
+            while True:
+                await asyncio.sleep(self.INACTIVITY_PERIOD_S)
+                for _ in range(3):
+                    await self._servo.move(self.UPPER_POSITION +
+                                           self.WIGGLE_AMOUNT)
+                    await asyncio.sleep(0.3)
+                    await self._servo.move(self.UPPER_POSITION)
+                    await asyncio.sleep(0.3)
+        except asyncio.CancelledError:
+            return
+
+    async def raise_hand(self):
+        """
+        Call this to move the servo to the raised position and start the task
+        that wiggles the hand on inactivity.
+        """
+        if self._wiggler is not None:
+            print("LOGIC BUG: trying to raise already-raised hand")
+            return
+        await self._servo.move(self.UPPER_POSITION)
+        self._wiggler = asyncio.create_task(self._wiggle_on_inactivity())
+
+    async def lower_hand(self):
+        """
+        Call this to move the servo to the lowered position and stop the
+        background task that wiggles the hand once in a while.
+        """
+        if self._wiggler is None:
+            print("LOGIC BUG: trying to lower already-lowered hand")
+            return
+        self._wiggler.cancel()
+        await self._wiggler
+        self._wiggler = None
+        await self._servo.move(self.LOWER_POSITION)
