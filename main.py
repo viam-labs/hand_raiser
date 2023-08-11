a56d53f9f629957695dc32568cf3e761310c748b
Alan Davidson <alan@viam.com>
2023-08-11 17:15:55 -0400

    Use a server to handle requests (#9)
    To run this yourself, you'll need to run `pip install -r requirements.txt` to install the [Quart](https://pypi.org/project/quart/) server framework (an async version of Flask). While you're running main.py in one terminal, you can open another terminal and run things like `curl -X POST localhost:8090/hand_count/5` and `curl -X POST localhost:8090/hand_count/0` to change how many hands are considered raised, which in turn affects whether the robot's hand is raised, too.
    
    With this change, hopefully we can count the number of raised hands in a Zoom meeting, and then post to this server to update the robot.
    
    
    * move Audience class to a separate file
    
    * Add requirements.txt, include quart server framework
    
    * use Quart to set the count of raised hands in the audience
    
    * make audience.py non-executable
    
    * Don't return the board from create_robot
    
    * debugging. whoops
    
    * add explanatory comment
    
    * rename endpoint

--- main.py
+++ main.py
@@ -1,78 +1,29 @@
 #!/usr/bin/env python3
 import asyncio
 
+from quart import Quart
+
+from audience import Audience
 from robot import create_robot
 import secrets
 
 
-class Audience:
-    def __init__(self, robot):
-        """
-        Audience keeps track of how many people have their hands up, and raises
-        and lowers the robot's hand to match. This class is thread safe.
-        """
-        self._robot = robot
-        self._mutex = asyncio.Lock()
-        self._count = 0  # Number of people in the audience raising their hands
-
-    async def increment_count(self):
-        """
-        Call this to consider 1 extra person in the audience to have raised
-        their hand. If this is the first person to do so, we'll raise the
-        robot's hand, and otherwise we take no action.
-        """
-        async with self._mutex:
-            self._count += 1
-            if self._count == 1:
-                await self._robot.raise_hand()
-
-    async def decrement_count(self):
-        """
-        Call this to consider 1 extra person in the audience to have lowered
-        their hand. If this is the last person who had their hand raised, we'll
-        lower the robot's hand, and otherwise take no action.
-        """
-        async with self._mutex:
-            self._count -= 1
-            if self._count == 0:
-                await self._robot.lower_hand()
-
-    # TODO: either test this thoroughly or remove it. It's currently unused.
-    async def set_count(self, new_value):
-        """
-        Call this to set the number of hands raised in the audience to a certain
-        value. This is mainly used to reset the count of raised hands if someone
-        forgets to lower their hand.
-        """
-        async with self._mutex:
-            if self._count == 0 and new_value > 0:
-                await self._robot.raise_hand()
-            if self._count > 0 and new_value == 0:
-                await self._robot.lower_hand()
-
-            self._count = new_value
+SERVER_PORT = 8090
 
 
 async def main():
-    async with create_robot(secrets.creds, secrets.address) as (robot, board):
+    async with create_robot(secrets.creds, secrets.address) as robot:
+        app = Quart(__name__)
         audience = Audience(robot)
-        button = await board.gpio_pin_by_name("18")
-        led = await board.gpio_pin_by_name("16")
 
-        should_raise = False
-        old_state = False
-        while True:
-            button_state = await button.get()
-            if button_state != old_state:
-                print("button state has changed to {}!".format(button_state))
-                if button_state:
-                    should_raise = not should_raise
-                    if should_raise:
-                        await audience.increment_count()
-                    else:
-                        await audience.decrement_count()
-            old_state = button_state
-            await led.set(button_state)
+        @app.route("/hand_count/<int:total>", methods=["POST"])
+        async def set_hand_count(total):
+            await audience.set_count(total)
+            return "Count has been set to {}\n".format(total)
+
+        # Use 0.0.0.0 to accept external requests, or 127.0.0.1 to accept only
+        # requests originating from within this computer.
+        await app.run_task(host="0.0.0.0", port=SERVER_PORT)
 
 
 if __name__ == "__main__":
