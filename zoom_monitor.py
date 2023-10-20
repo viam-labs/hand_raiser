82358ba2e1e7e3dd7c2625dc8a603cf54e091cf0
Alan Davidson <alan@viam.com>
2023-10-20 17:19:05 -0400

    reflow comment: Python has an 80-character max line length

--- zoom_monitor.py
+++ zoom_monitor.py
@@ -46,10 +46,12 @@ class ZoomMonitor():
         (so you definitely join the meeting inside the browser that Selenium
         has opened).
         """
-        # On certain unusual shells, when you paste a URL, it automatically escapes the question
-        # mark symbol so the shell doesn't try to pattern-match on files in the file system. If you
-        # put the URL in quotes and still get those escapes, Zoom won't be able to find the
-        # passcode in the URL. So, in here, we first must remove all backslashes.
+        # On certain unusual shells, when you paste a URL, it automatically
+        # escapes the question mark symbol so the shell doesn't try to
+        # pattern-match on files in the file system. If you put the URL in
+        # quotes and still get those escapes, Zoom won't be able to find the
+        # passcode in the URL. So, in here, we first must remove all
+        # backslashes.
         url = url.replace("\\", "")
 
         # Google Calendar wraps its links in a redirect. Check for that first
