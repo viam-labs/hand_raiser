f51acd7a557bc31c6f4bd3f1c0964ecc2b300b4d
Alan Davidson <alan@viam.com>
2023-10-24 14:57:17 -0400

    Tweaks (#18)
    * Put all public functions at the top of the Robot class
    
    * rename ZoomMonitor._sign_in -> _join_meeting
    
    * rename updated_url -> raw_url
    
    * add docstrings to ZoomMonitor class, tweak comment
    
    * Add readme
    
    * remove systemd service config: no longer used
    
    * rephrase comment
    
    * fix merge conflict leftovers
    
    * remove backslashes from URL
    
    * exit zoom when shutting down. Add TODO that this doesn't work after a control-C
    
    * reflow comment: Python has an 80-character max line length
    
    * Update README.md
    
    Co-authored-by: JessamyT <75634662+JessamyT@users.noreply.github.com>
    
    * Update README.md
    
    Co-authored-by: JessamyT <75634662+JessamyT@users.noreply.github.com>
    
    * Update README.md
    
    Co-authored-by: JessamyT <75634662+JessamyT@users.noreply.github.com>
    
    * Update README.md
    
    Co-authored-by: JessamyT <75634662+JessamyT@users.noreply.github.com>
    
    * Update README.md
    
    Co-authored-by: JessamyT <75634662+JessamyT@users.noreply.github.com>
    
    * Update README.md
    
    Co-authored-by: JessamyT <75634662+JessamyT@users.noreply.github.com>
    
    * Update README.md
    
    Co-authored-by: JessamyT <75634662+JessamyT@users.noreply.github.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: Nicole Jung <31713368+purplenicole730@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: JessamyT <75634662+JessamyT@users.noreply.github.com>
    Co-authored-by: Nicole Jung <31713368+purplenicole730@users.noreply.github.com>

---
+++ README.md
@@ -0,0 +1,26 @@
+# Hand Raiser robot for Zoom meetings
+
+When we have large meetings, sometimes the remote employees are unable to participate in the Q&A section at the end, even though they're able to view the main meeting over Zoom. This repo provides a way for a robot to join the Zoom meeting and raise a physical hand in the (meatspace) meeting room when someone on Zoom has a question.
+
+## Installation
+
+1. Clone this repo locally.
+2. Run `pip install -r requirements.txt` to install the dependencies.
+3. Edit `secrets.py` so it contains the robot's secret and URL. You can get these from anyone who worked on this project.
+4. Step 3 probably made the repo dirty. Run `git update-index --skip-worktree secrets.py` to make the repo clean again.
+
+## Running
+1. When you want Hand Raiser Bot to join a meeting, run `./main.py '<url-of-zoom-meeting>'` to start it. The URL likely contains a question mark, so we recommend enclosing the entire URL in single quotes so your terminal doesn't try pattern-matching on it.
+2. This will open a Chrome window and join the Zoom meeting as the user "Hand Raiser Bot."
+3. Whenever someone in the Zoom meeting selects the "Raise Hand" reaction, the servo on the robot moves. The hand will be raised whenever _at least 1_ person in the Zoom meeting has their hand raised, and lowered again when no one has their hand raised.
+4. If the hand has been raised for at least 30 seconds, it will begin to wiggle side-to-side at intervals to try to gain attention.
+5. Once a Zoom participant has been called upon, they should lower their hand in the Zoom interface! This will lower the robot hand, unless other meeting participants also have raised hands in Zoom.
+6. When the meeting is over (or when you want Hand Raiser Bot to leave), hit control-C in the terminal to shut everything down. This will also lower the servo even if someone in the Zoom meeting still has their hand raised.
+
+## Code Layout
+- `robot.py` talks to `viam-server` to move the hardware itself. The code in this file raises and lowers the servo, and wiggles it if it has been raised for a while.
+- `audience.py` keeps track of how many hands are raised. This tells the robot when it's time to raise and lower the hand.
+- `zoom_monitor.py` uses Selenium to open a web browser and join the Zoom meeting. It counts how many participants in the meeting have their hands raised.
+  - As of summer 2023, Zoom did not have an official API for participart reactions like whether someone has raised their hand. Consequently, we're getting this data by webscraping with Selenium.
+- `secrets.py` contains the way to connect to the robot itself. This repo does not contain production data: this file must be edited before things will work.
+- `main.py` ties everything together: it sets up a ZoomMonitor, connects to a robot, wraps the robot in an Audience object, and then sets the hand count in the Audience based on what is reported from the ZoomMonitor.
--- zoom_monitor.py
+++ zoom_monitor.py
@@ -20,21 +20,40 @@ def monitor_zoom(url, log_level):
 
 
 class ZoomMonitor():
+    """
+    Given a URL to a Zoom meeting, join the meeting using Selenium controlling
+    a Chrome browser. We provide a way to count how many meeting participants
+    currently have their hands raised.
+    """
     def __init__(self, url, log_level):
         self._logger = getLogger(__name__)
         setLevel(log_level)
 
         self._driver = Chrome()
 
-        updated_url = self._get_updated_url(url)
-        self._logger.debug(f"updated URL {url} to {updated_url}")
-        self._driver.get(updated_url)
+        raw_url = self._get_raw_url(url)
+        self._logger.debug(f"parsed URL {url} to {raw_url}")
+        self._driver.get(raw_url)
 
-        self._sign_in()
+        self._join_meeting()
         self._open_participants_list()
 
     @staticmethod
-    def _get_updated_url(url):
+    def _get_raw_url(url):
+        """
+        Remove any Google redirection to the Zoom meeting, and then return a
+        URL that should skip Zoom prompting you to open the link in their app
+        (so you definitely join the meeting inside the browser that Selenium
+        has opened).
+        """
+        # On certain unusual shells, when you paste a URL, it automatically
+        # escapes the question mark symbol so the shell doesn't try to
+        # pattern-match on files in the file system. If you put the URL in
+        # quotes and still get those escapes, Zoom won't be able to find the
+        # passcode in the URL. So, in here, we first must remove all
+        # backslashes.
+        url = url.replace("\\", "")
+
         # Google Calendar wraps its links in a redirect. Check for that first
         # and remove it if relevant. The "real" URL is stored in the `q`
         # parameter in the CGI arguments.
@@ -43,14 +62,16 @@ class ZoomMonitor():
             cgi_params = urllib.parse.parse_qs(parsed_url.query)
             url = cgi_params["q"][0]
 
-        # The links that we receive from Zoom prompt you to open the Zoom app
-        # if it's available. Replace the domain name and first couple
+        # Many links that we receive from Zoom will prompt you to open the
+        # Zoom app if it's available. Replace the domain name and first couple
         # directories in the path to skip that.
         return f"https://app.zoom.us/wc/join/{url.split('/')[-1]}"
 
-    def _sign_in(self):
+    def _join_meeting(self):
+        """
+        Set our name and join the meeting. This function returns nothing.
+        """
         self._logger.debug("logging in...")
-        # Set our name and join the meeting
         self._wait_for_element(By.ID, "input-for-name")
         self._driver.find_element(By.ID, "input-for-name").send_keys(
             "Hand Raiser Bot")
@@ -58,6 +79,10 @@ class ZoomMonitor():
         self._logger.info("logged into Zoom successfully")
 
     def _open_participants_list(self):
+        """
+        Wait until we can open the participants list, then open it, then wait
+        until it's opened. This function returns nothing.
+        """
         self._wait_for_element(By.CLASS_NAME, "SvgParticipantsDefault")
         # There's something else we're supposed to wait for, but we can't
         # figure out what. So, instead let's just try to continue, and retry a
@@ -67,11 +92,11 @@ class ZoomMonitor():
             # to open the participants list. However, that element is not
             # clickable, and instead throws an exception that the click would
             # be intercepted by its parent element, a div in the class
-            # "footer-button-base__img-layer". So, we'd like to find that
-            # element and then click on its parent element. but it's not obvious
-            # how to do that in Selenium. So, instead let's look for all of
-            # those divs, and then find the one that contains the participants
-            # image.
+            # "footer-button-base__img-layer". So, we'd like to find that SVG
+            # element and then click on its parent. But it's not obvious how to
+            # do that in Selenium. So, instead let's look for all of those
+            # footer divs, and then click on the one that contains the
+            # participants image.
             for outer in self._driver.find_elements(
                     By.CLASS_NAME, "footer-button-base__img-layer"):
                 try:
@@ -100,13 +125,35 @@ class ZoomMonitor():
             f"Could not open participants list after {attempt + 1} attempts")
 
     def _wait_for_element(self, approach, value):  # Helper function
+        """
+        Wait until there is at least one element identified by the approach
+        and value. If 5 seconds elapse without such an element appearing, we
+        raise an exception.
+        """
         WebDriverWait(self._driver, 5).until(lambda _:
             len(self._driver.find_elements(approach, value)) != 0)
 
     def clean_up(self):
-        self._driver.quit()
+        """
+        Leave the meeting and shut down the web server.
+        """
+        try: # If anything goes wrong, close the browser anyway.
+            # Find the "leave" button and click on it.
+            # TODO: this next line raises a urllib3.exceptions.MaxRetryError if
+            # called after you hit control-C to kill everything, but quits Zoom
+            # correctly if this gets called without hitting control-C. See if
+            # there's a way to get it to leave the Zoom room no matter what.
+            self._driver.find_element(
+                By.CLASS_NAME, "footer__leave-btn").click()
+            self._driver.find_element(
+                By.CLASS_NAME, "leave-meeting-options__btn").click()
+        finally:
+            self._driver.quit()
 
     def count_hands(self):
+        """
+        Return the number of people in the participants list with raised hands
+        """
         # We want to find an SVG element whose class is
         # "lazy-svg-icon__icon lazy-icon-nvf/270b". However,
         # `find_elements(By.CLASS_NAME, ...)` has problems when the class name
