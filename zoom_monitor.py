fb86ac8fe1454914f3171d49ea8008b648510c3c
Alan Davidson <alan@viam.com>
2024-05-09 14:20:24 -0400

    get Chromium to work even if another Chromium window is already opened

--- zoom_monitor.py
+++ zoom_monitor.py
@@ -1,6 +1,7 @@
 from contextlib import contextmanager
 import functools
 import os
+import socket
 import subprocess
 import sys
 import time
@@ -47,7 +48,18 @@ class ZoomMonitor():
         setLevel(log_level)
 
         chrome_options = Options()
-        chrome_options.add_argument("--headless=new")
+        #chrome_options.add_argument("--headless=new")
+
+        # Chromium can hang if something else is using its default remote
+        # debugging port (e.g., if you've got another Chromium window open at
+        # the same time. So, we give our Chromium window a brand new, ephemeral
+        # port. This solution was taken from Jingyu Lei's comment on
+        # https://stackoverflow.com/q/60151593
+        sock=socket.socket()
+        sock.bind(("", 0))
+        port = sock.getsockname()[1]
+        chrome_options.add_argument(f"--remote-debugging-port={port}")
+
         # Uncomment this next line to keep the browser open even after this
         # process exits. It's a useful option when debugging or adding new
         # features, though it's most useful when you comment out the previous
