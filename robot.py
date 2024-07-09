d3731035808602846c071be876d26db22a30ba2b
JessamyT <75634662+JessamyT@users.noreply.github.com>
2024-07-09 13:13:07 -0700

    Make waving less obtrusive (#32)

--- robot.py
+++ robot.py
@@ -35,7 +35,7 @@ class Robot:
     LOWER_POSITION = 152
     WIGGLE_AMOUNT = 7  # Move this much left and right of UPPER_POSITION
     WIGGLE_DELAY_S = 0.5
-    INACTIVITY_PERIOD_S = 30
+    INACTIVITY_PERIOD_S = 60
 
     def __init__(self, servo, log_level):
         """
@@ -103,14 +103,14 @@ class Robot:
             while True:
                 await asyncio.sleep(self.INACTIVITY_PERIOD_S)
                 self._logger.debug("wiggle wiggle wiggle")
-                for _ in range(3):
+                for _ in range(1):
                     await self._servo.move(self.UPPER_POSITION +
                                            self.WIGGLE_AMOUNT)
                     await asyncio.sleep(self.WIGGLE_DELAY_S)
                     await self._servo.move(self.UPPER_POSITION -
                                            self.WIGGLE_AMOUNT)
                     await asyncio.sleep(self.WIGGLE_DELAY_S)
-                # Now that we're done wiggle for now, put the arm back up.
+                # Now that we're done wiggling for now, put the arm back up.
                 self._logger.debug("stop wiggling")
                 await self._servo.move(self.UPPER_POSITION)
         except asyncio.CancelledError:
