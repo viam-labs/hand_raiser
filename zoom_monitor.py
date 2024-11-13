a4e40f8967f8b550d1b2361a481a520f97091757
Alan Davidson <alan@viam.com>
2024-11-13 17:26:09 -0500

    pr feedback: remove useless comment

--- zoom_monitor.py
+++ zoom_monitor.py
@@ -87,11 +87,11 @@ class ZoomMonitor():
         self._wait_for_element(By.XPATH, PARTICIPANTS_BTN, timeout_s=30)
         self._logger.info("logged into Zoom successfully")
 
-    def _wait_for_element(self, approach, value, timeout_s=5):  # Helper fn
+    def _wait_for_element(self, approach, value, timeout_s=5):
         """
         Wait until there is at least one element identified by the approach
         and value. If `timeout_s` seconds elapse without such an element
-        appearing, we raise an exception.
+        appearing, we raise a TimeoutException.
         """
         WebDriverWait(self._driver, timeout_s).until(lambda _:
             len(self._driver.find_elements(approach, value)) != 0)
