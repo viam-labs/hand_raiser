6f8d9497dcbae3a38d9a3024b327250ef6d946a1
Alan Davidson <alan@viam.com>
2024-11-15 16:05:11 -0500

    start using argparse, so it's easier to remember the log levels

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
+import argparse
 import asyncio
+import logging
 import sys
 
 from audience import Audience
@@ -7,9 +9,17 @@ from robot import create_robot
 from zoom_monitor import monitor_zoom, MeetingEndedException
 
 
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("url", help="URL of Zoom meeting. Can be a Google redirect.")
+    parser.add_argument("--debug", help="Turn on debugging logs", action="store_true")
+    return parser.parse_args()
+
+
 async def main():
-    log_level = int(sys.argv[2]) if len(sys.argv) == 3 else 20
-    with monitor_zoom(sys.argv[1], log_level) as zoom:
+    args = parse_args()
+    log_level = logging.DEBUG if args.debug else logging.INFO
+    with monitor_zoom(args.url, log_level) as zoom:
         async with create_robot(log_level) as robot:
             audience = Audience(robot, log_level)
 
