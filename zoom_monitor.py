6c204d9bb9a13bf7e69f2305b4f6f41b08ca625e
purplenicole730 <jihyun.nicole.jung@gmail.com>
2025-06-17 15:26:18 -0400

    make comment more clear

--- zoom_monitor.py
+++ zoom_monitor.py
@@ -8,8 +8,8 @@ from viam.logging import getLogger, setLevel
 try:
     import browser
 except ImportError:
-    # For testing since it can't find the browser package, and running main
-    # complains about not knowing the parent package.
+    # For testing since the test can't find the browser package, and running
+    # main doesn't know the parent package when run with this.
     from . import browser
 
 
